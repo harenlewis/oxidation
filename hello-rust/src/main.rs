31a4704742f67e4c9e733eb7bb8ad5ed8b6f52e3
harenlewis <harenlewis@gmail.com>
2020-05-23 23:36:00 +0530

    primitive types.

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,7 +1,15 @@
+use std::mem;
+
 fn main() {
     println!("Hello, world!");
     println!("Some cool print statement");
 
     let x = 123;
     println!("{}", x);
+
+    let a: [i32; 3] = [1, 2, 3] ;
+    println!("{:?} {} {}", a, a[2], mem::size_of_val(&a));
+
+    let s = "Slice string";
+    println!("{}", s);
 }
