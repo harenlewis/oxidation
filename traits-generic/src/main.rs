8666424a05dcee956404377dfbd9bbfd7c005c97
harenlewis <harenlewis@gmail.com>
2020-05-29 23:48:40 +0530

    Added trait example.

--- traits-generic/src/main.rs
+++ traits-generic/src/main.rs
@@ -1,3 +1,26 @@
+trait Shape {
+    fn area(&self) -> u32;
+}
+
+struct Rectangle {
+    x: u32,
+    y: u32,
+}
+
+struct Circle {
+    radius: f64,
+}
+
+struct Square {
+    x: u32,
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> u32 {
+        self.x * self.y
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
