d1e6abb8cc33abc1284167a5af8729ee59918700
harenlewis <harenlewis@gmail.com>
2020-05-29 23:57:37 +0530

    Added trait example.

--- traits-generic/src/main.rs
+++ traits-generic/src/main.rs
@@ -17,6 +17,11 @@ impl Shape for Rectangle {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct A(i32);
+
+struct B(f32);
+
 impl Shape for Circle {
     fn area(&self) -> u32 {
         (3.14 * self.radius * self.radius) as u32
@@ -30,4 +35,11 @@ fn main() {
     let r = Rectangle {x: 10, y: 15};
 
     println!("{} {}", c.area(), r.area());
+
+    let a = A(32);
+    let b = B(10.5);
+    let c = a;
+
+    println!("{:?}", a);
+
 }
